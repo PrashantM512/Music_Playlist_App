f5220737685cf1f763b49b38d42f46e5c529d688
PrashantM512 <prashantmorade512@gmail.com>
2023-08-20 18:10:10 +0530

    Fisrt Push

---
+++ src/com/musicplayer/Main.java
@@ -0,0 +1,10 @@
+package com.musicplayer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Music_Playlist_App {
+}
